98b1bae16be9415d0b9ca5a7147be39ba5359af4
William Getz <willegetz@gmail.com>
2021-04-22 08:03:54 -0700

    First rest for characteristics table

---
+++ RandomTables/RandomTables/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RandomTables")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RandomTables")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb98293c-546f-4f93-974a-88fd2a94a30b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RandomTables/RandomTables/WorldHooks/Characteristic.cs
@@ -0,0 +1,51 @@
+﻿using DiceTypes.DieTypes;
+using IntervalTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace RandomTables.WorldHooks
+{
+    [TestClass]
+    public class Characteristic
+    {
+
+        // The characteristics are in a collection assigned to a range of numbers.
+        // If the percentile dice 0 and 0 return a result between the upper and lower
+        // bounds of the range, inclusive, then that characteristic is chosen.
+        // The full range goes from 01 to 00.
+
+        // Climate or landform is chosen when the result is 01 to 24.
+        // Random seed: Guid.NewGuid().GetHashCode();
+
+        private IntervalTree<int, string> characteristicsTable;
+
+        [TestInitialize]
+        public void SeedIntervalTree()
+        {
+            characteristicsTable = new IntervalTree<int, string>()
+            {
+                {1, 24, "Climate or Landform" }
+            };
+        }
+
+        [TestMethod]
+        public void ClimateOrLandformWhen01IsRolled()
+        {
+            var seedGenerates0 = 14;
+            var seedGenerates1 = 18;
+
+            var percentile = new PercentileDice0and0(seedGenerates0, seedGenerates1);
+            var rollResult = percentile.RollDice();
+
+            var characteristic = characteristicsTable.Query(rollResult)
+                                            .ToList()
+                                            .Select(x => x)
+                                            .FirstOrDefault();
+
+            var expectedCharacteristic = "Climate or Landform";
+
+            Assert.AreEqual(expectedCharacteristic, characteristic);
+        }
+    }
+}
