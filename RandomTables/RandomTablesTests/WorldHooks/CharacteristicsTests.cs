e4c6f09dd5ad5459cb7e02ee3b777b55fa5a5a52
William Getz <willegetz@gmail.com>
2021-04-25 15:13:18 -0700

    Moved exploratory tests to new file

--- RandomTables/RandomTablesTests/WorldHooks/CharacteristicsTests.cs
+++ RandomTables/RandomTablesTests/WorldHooks/CharacteristicsTests.cs
@@ -1,11 +1,5 @@
-﻿using IntervalTree;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using RandomTables.Interfaces;
-using RandomTables.Interfaces.WorldHooks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RandomTables.WorldHooks;
-using RandomTables.WorldHooks.Types;
-using System;
-using System.Linq;
 
 namespace RandomTablesTests.WorldHooks
 {
@@ -140,71 +134,6 @@ namespace RandomTablesTests.WorldHooks
             var expectedCharacteristic = "Historical";
 
             Assert.AreEqual(expectedCharacteristic, characteristic);
-        }
-
-
-        private IntervalTree<int, IFactory> worldHookFactoryTable = new IntervalTree<int, IFactory>()
-        {
-            {1, 24, new SpikeClimateOrLandformFactory() },
-            {25, 34, new SpikeSitesOfInterestFactory() }
-        };
-
-        private IntervalTree<int, IWorldHookSubtype> worldHookSubtypesTable = new IntervalTree<int, IWorldHookSubtype>()
-        {
-            {1, 24, new SpikeClimateOrLandformFactory().Get() },
-            {25, 34, new SpikeSitesOfInterestFactory().Get() }
-        };
-
-        public int GetResult()
-        {
-            var seed = Guid.NewGuid().GetHashCode();
-            return new Random(seed).Next(1, 35);
-        }
-
-        [TestMethod]
-        [Ignore("Experimenting with factory")]
-        public void AUseFactoryTable()
-        {
-            var result = GetResult();
-
-            var worldHookFactory = worldHookFactoryTable.Query(result).ToList().FirstOrDefault();
-            var hookSubtype = worldHookFactory.Get();
-            var hookType = hookSubtype.GetHook();
-
-            Assert.AreEqual(hookType, "");
-        }
-
-        [TestMethod]
-        [Ignore("Experimenting with factory")]
-        public void AUseSubtypeTable()
-        {
-            var result = GetResult();
-
-            var worldHooks = worldHookSubtypesTable.Query(result).ToList().FirstOrDefault();
-            var hookType = worldHooks.GetHook();
-
-            Assert.AreEqual(hookType, "");
-        }
-    }
-
-    public class SpikeClimateOrLandformFactory : IFactory
-    {
-        public IWorldHookSubtype Get()
-        {
-            var d8Seed = Guid.NewGuid().GetHashCode();
-            var d6Seed = Guid.NewGuid().GetHashCode();
-
-            return new ClimateOrLandform(d8Seed, d6Seed);
-        }
-    }
-
-    public class SpikeSitesOfInterestFactory : IFactory
-    {
-        public IWorldHookSubtype Get()
-        {
-            var d8Seed = Guid.NewGuid().GetHashCode();
-
-            return new SitesOfInterest(d8Seed);
         }
     }
 }
