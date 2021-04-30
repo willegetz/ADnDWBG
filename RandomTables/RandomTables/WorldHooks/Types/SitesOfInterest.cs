b44946949d603349818be39dfc09bf96f4c4abf5
William Getz <willegetz@gmail.com>
2021-04-29 22:19:12 -0700

    Finish base class implementation for sites of interest

--- RandomTables/RandomTables/WorldHooks/Types/SitesOfInterest.cs
+++ RandomTables/RandomTables/WorldHooks/Types/SitesOfInterest.cs
@@ -1,33 +1,16 @@
 ﻿using DiceTypes.Interfaces;
-using RandomTables.Interfaces.WorldHooks;
 using RandomTables.WorldHooks.Subtypes;
 
 namespace RandomTables.WorldHooks.Types
 {
-    public class SitesOfInterest : BaseCharacteristicType, IWorldHook
+    public class SitesOfInterest : BaseCharacteristicType
     {
-        public string CharacteristicType { get { return "Sites of Interest"; } }
-
-        public string Subtype { get { return _subtype.GetCharacteristicSubtype(); } }
-
-        private SitesOfInterestSubtype _subtype;
-
         public SitesOfInterest() : base("Sites of Interest", new SitesOfInterestSubtype())
         {
-            _subtype = new SitesOfInterestSubtype();
         }
 
         public SitesOfInterest(ISeedGenerator seedGenerator): base("Sites of Interest", new SitesOfInterestSubtype(seedGenerator))
         {
-            _subtype = new SitesOfInterestSubtype(seedGenerator);
-        }
-
-        public string GetHook()
-        {
-            var subtype = _subtype.GetCharacteristicSubtype();
-
-            return $@"Characteristic: {CharacteristicType}
-Subtype: {subtype}";
         }
     }
 }
