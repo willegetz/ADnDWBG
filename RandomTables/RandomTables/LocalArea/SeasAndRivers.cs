61e961109501b21c3d7f3ad53aad0a16713771f9
William Getz <willegetz@gmail.com>
2021-05-05 17:46:42 -0700

    Begin to scaffold the seas and rivers lookup

---
+++ RandomTables/RandomTables/LocalArea/SeasAndRivers.cs
@@ -0,0 +1,36 @@
+﻿using DiceTypes.DieTypes.Complex;
+using DiceTypes.Interfaces;
+using IntervalTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomTables.LocalArea
+{
+    public class SeasAndRivers
+    {
+        private PercentileDice0and0 _percentileDice;
+
+        public IntervalTree<int, string> seasAndRiversLookup = new IntervalTree<int, string>()
+        {
+            {1, 10, "Island" }
+        };
+
+        public SeasAndRivers(ISeedGenerator seedGenerator)
+        {
+            _percentileDice = new PercentileDice0and0(seedGenerator);
+        }
+
+        public string GetLocalAreaDetail()
+        {
+            var rollResult = _percentileDice.RollDie();
+            var result = seasAndRiversLookup.Query(rollResult)
+                                            .ToList()
+                                            .Select(x => x)
+                                            .FirstOrDefault();
+            return result;
+        }
+    }
+}
---
+++ RandomTables/RandomTablesTests/LocalArea/SeasAndRiveresTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomTables.LocalArea;
+using RandomTables.TestHelpers;
+
+namespace RandomTablesTests.LocalArea
+{
+    [TestClass]
+    public class SeasAndRiveresTests
+    {
+        private const int d10Returns0 = 14;
+        private const int d10Returns1 = 18;
+
+        [TestMethod]
+        public void IslandWhen01IsRolled()
+        {
+            // Using two d10s each with face numbers 0-9, assign one as the "tens" value
+            // Roll them and consult the "seas and rivers" table
+            // A roll of "01" will result in an Island
+            // An island is typically 20 to 40 miles across and could also contain
+            // minor lakes, streams, or other small bodies of water
+
+            // For this test, we are more interested in ensuring the table being
+            // built will give us back the appropriate result
+            // Any additional details will be implemented later
+
+            var tensSeed = d10Returns0;
+            var onesSeed = d10Returns1;
+
+            var seeds = new[] { tensSeed, onesSeed };
+            var mockSeedGenerator = SeedHelper.GetMockSeedGenerator(seeds);
+
+            var seasAndRivers = new SeasAndRivers(mockSeedGenerator.Object);
+            var seaAndRiverResult = seasAndRivers.GetLocalAreaDetail();
+
+            var expectedseaAndRiverResult = "Island";
+
+            Assert.AreEqual(expectedseaAndRiverResult, seaAndRiverResult);
+        }
+    }
+}
