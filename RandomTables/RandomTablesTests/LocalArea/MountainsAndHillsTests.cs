6cd3531c32067a95848bc9340c5f8e4d7b30bf6f
William Getz <willegetz@gmail.com>
2021-05-06 17:46:15 -0700

    Upper and lower bound tests for result of gentle hills

--- RandomTables/RandomTablesTests/LocalArea/MountainsAndHillsTests.cs
+++ RandomTables/RandomTablesTests/LocalArea/MountainsAndHillsTests.cs
@@ -12,8 +12,9 @@ namespace RandomTablesTests.LocalArea
         private const int d10Returns1 = 18;
         private const int d10Returns2 = 1;
         private const int d10Returns3 = 5;
-        private const int d10Returns8 = 4;
+        private const int d10Returns6 = 17;
         private const int d10Returns7 = 0;
+        private const int d10Returns8 = 4;
         private const int d10Returns9 = 8;
 
         [TestMethod]
@@ -114,6 +115,40 @@ namespace RandomTablesTests.LocalArea
             var mountainsAndHillsResult = mountainsAndHills.GetLocalAreaDetail();
 
             var expectedMountainsAndHillsResult = "Rugged Hills";
+
+            Assert.AreEqual(expectedMountainsAndHillsResult, mountainsAndHillsResult);
+        }
+
+        [TestMethod]
+        public void GentleHillsWhen38IsRolled()
+        {
+            var tensSeed = d10Returns3;
+            var onesSeed = d10Returns8;
+
+            var seeds = new[] { tensSeed, onesSeed };
+            var mockSeedGenerator = SeedHelper.GetMockSeedGenerator(seeds);
+
+            var mountainsAndHills = new MountainsAndHills(mockSeedGenerator.Object);
+            var mountainsAndHillsResult = mountainsAndHills.GetLocalAreaDetail();
+
+            var expectedMountainsAndHillsResult = "Gentle Hills";
+
+            Assert.AreEqual(expectedMountainsAndHillsResult, mountainsAndHillsResult);
+        }
+
+        [TestMethod]
+        public void GentleHillsWhen69IsRolled()
+        {
+            var tensSeed = d10Returns6;
+            var onesSeed = d10Returns9;
+
+            var seeds = new[] { tensSeed, onesSeed };
+            var mockSeedGenerator = SeedHelper.GetMockSeedGenerator(seeds);
+
+            var mountainsAndHills = new MountainsAndHills(mockSeedGenerator.Object);
+            var mountainsAndHillsResult = mountainsAndHills.GetLocalAreaDetail();
+
+            var expectedMountainsAndHillsResult = "Gentle Hills";
 
             Assert.AreEqual(expectedMountainsAndHillsResult, mountainsAndHillsResult);
         }
