e5a1e33b865b6c761c6f8851014bced97e6196fa
William Getz <willegetz@gmail.com>
2021-05-10 08:21:20 -0700

    Add prevailing winds lookup

---
+++ RandomTables/RandomTables/ContinentsAndGeography/PrevailingWinds.cs
@@ -0,0 +1,30 @@
+﻿using DiceTypes.DieTypes.Basic;
+using DiceTypes.Interfaces;
+using RandomTables.BaseClasses.WorldHooks;
+using System.Collections.Generic;
+
+namespace RandomTables.ContinentsAndGeography
+{
+    public class PrevailingWinds : BaseLookupTable
+    {
+        private Dictionary<int, string> prevailingWindsLookup = new Dictionary<int, string>()
+        {
+            { 1, "North" },
+            { 2, "Northeast" },
+            { 3, "East" },
+            { 4, "Southeast" },
+            { 5, "South" },
+            { 6, "Southwest" },
+            { 7, "West" },
+            { 8, "Northwest" }
+        };
+
+        public PrevailingWinds() : base(new D8())
+        {
+            base.LookupTable = prevailingWindsLookup;
+        }
+        public PrevailingWinds(ISeedGenerator seedGenerator) : base(new D8(seedGenerator))
+        {
+            base.LookupTable = prevailingWindsLookup;
+        }
+    }
